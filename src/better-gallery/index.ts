35e9ba5881846361f72208273fbe95d7abf0abf6
MontejoJorge <jorgemon.lopez@gmail.com>
2026-02-23 20:35:21 +0100

    fix typecheck

--- src/better-gallery/index.ts
+++ src/better-gallery/index.ts
@@ -1,12 +1,10 @@
 import { registerBlockType } from '@wordpress/blocks';
-
-import './style.scss';
-
 import metadata from './block.json';
 import Edit from './edit';
 import save from './save';
+import './style.scss';
 
-registerBlockType(metadata.name, {
+registerBlockType(metadata.name as any, {
   edit: Edit,
   save,
 });
